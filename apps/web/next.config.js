bd4b88414da0792085858121a3d6fc297eb05089
virginus01 <info.vsolace@gmail.com>
2025-08-07 18:18:37 +0100

    uhhd

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has type errors.
+        ignoreBuildErrors: false,
+    },
+};
 
 export default nextConfig;
