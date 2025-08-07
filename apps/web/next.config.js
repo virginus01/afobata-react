c263ddb9447d4dedeb4732f1588edad89b295b5b
virginus01 <info.vsolace@gmail.com>
2025-08-07 18:23:53 +0100

    kj

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
@@ -9,8 +8,9 @@ const nextConfig = {
     typescript: {
         // Warning: This allows production builds to successfully complete even if
         // your project has type errors.
-        ignoreBuildErrors: false,
+        ignoreBuildErrors: true,
     },
+    reactStrictMode: true,
 };
 
 export default nextConfig;
